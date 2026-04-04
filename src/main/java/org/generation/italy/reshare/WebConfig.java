80c8a3d70c2101421ca3ad176f76ccfc729ad839
Stanislao Tariffa <stany.tariffa@gmail.com>
2026-04-04 20:29:32 +0200

    s

--- src/main/java/org/generation/italy/reshare/WebConfig.java
+++ src/main/java/org/generation/italy/reshare/WebConfig.java
@@ -1,11 +1,28 @@
 package org.generation.italy.reshare;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.concurrent.TimeUnit;
+
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:C:/Dev/ProjectWork/Reshare/uploads/")
+                .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS).cachePublic());
     }
 }
